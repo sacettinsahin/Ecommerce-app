0520e7faed64cf3beed32d59cc8d41ec4644e12b
Sacettin Şahin <sacettin.sahin@etiya.com>
2023-05-07 08:58:53 +0300

    User model has been created

---
+++ backend/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  _id: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: Boolean,
+  createdDate: Date,
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
